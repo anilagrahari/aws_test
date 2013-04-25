19dd15acd78eb31b6f0a8a0bdaedca402f1edefe
anilagrahari <anil.agrahari@gmail.com>
2013-04-25 18:36:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <head>
   <meta charset="utf-8">
   <title>Demo</title>
-  <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /> 
-</head>
+  </head>
 <body> 
-hello body h
+hi
 </body>
 </html>
